29eeab943d03b3e7691c9ccd6da9814c19a4a8ef
mehe0005 <islam.mehenni-meghraoui@etudiant.univ-reims.fr>
2026-03-08 18:03:18 +0100

    profil page

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navigation } from "./components/Navigation";
 import { Footer } from "./components/Footer";
-import { Home } from "./pages/Home";
-import { About } from "./pages/About"; // NOUVELLE IMPORTATION
-import { Skills } from "./pages/Skills";
-import { Projects } from "./pages/Projects";
+import { Home } from "./components/Home";
+import { About } from "./components/About"; // NOUVELLE IMPORTATION
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
 import NotFound from "./pages/NotFound";
 
 const App = () => {
